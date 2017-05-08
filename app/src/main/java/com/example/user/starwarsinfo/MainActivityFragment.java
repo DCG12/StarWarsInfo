f9f4fe0d1371be889ad93c90776768c819db5807
David <46406163y@iespoblenou.org>
2017-05-08 11:49:12 +0200

    Creació del list View

--- app/src/main/java/com/example/user/starwarsinfo/MainActivityFragment.java
+++ app/src/main/java/com/example/user/starwarsinfo/MainActivityFragment.java
@@ -5,11 +5,18 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+
+    private ArrayList<String> items;
+    private ArrayAdapter<String> adapter;
 
     public MainActivityFragment() {
     }
@@ -17,6 +24,19 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        ListView lvPelis = (ListView) view.findViewById(R.id.lvWars);
+
+        items = new ArrayList<>();
+        adapter = new ArrayAdapter<>(
+                getContext(),
+                R.layout.lv_wars_row,
+                R.id.tvWars,
+                items
+        );
+        lvPelis.setAdapter(adapter);
+
+        return view;
     }
 }
